7636cc984539ebe46c9b172c92db38c84dada991
Pramod Topannavar <122197555+Pramodtopannavar@users.noreply.github.com>
2024-11-08 15:43:41 +0530

    Update settings.py

--- MyRemoteDesk/settings.py
+++ MyRemoteDesk/settings.py
@@ -159,5 +159,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "pramodtopannavar17@gmail.com"
-EMAIL_HOST_PASSWORD = "sdpx cuwl dirj eovb"
+EMAIL_HOST_USER = "xyz.com"
+EMAIL_HOST_PASSWORD = "sdpx cuwl"
